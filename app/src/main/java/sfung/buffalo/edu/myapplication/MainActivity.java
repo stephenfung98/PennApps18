954d05e044908f968d8e036ee99b5ae9537ee7a7
jayll <jay.lligui@gmail.com>
2018-01-25 17:11:38 -0500

    alpha 1.4

--- app/src/main/java/sfung/buffalo/edu/myapplication/MainActivity.java
+++ app/src/main/java/sfung/buffalo/edu/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // hello
+        //hi
     }
 
 }
