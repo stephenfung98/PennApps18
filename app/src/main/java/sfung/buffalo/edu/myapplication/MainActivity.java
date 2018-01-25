a690251b848a6ecff3c5b1505e6db1a029761a2d
jayll <jay.lliguichushca@gmail.com>
2018-01-25 02:00:40 -0500

    alpha 1

--- app/src/main/java/sfung/buffalo/edu/myapplication/MainActivity.java
+++ app/src/main/java/sfung/buffalo/edu/myapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package sfung.buffalo.edu.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
